25b43259f375cf124d341667d1fee046eb0219af
Felix Cherubini <felix.cherubini@softwareschneiderei.de>
2020-12-16 15:35:06 +0100

    Refactor and adjust parsing

--- source/uncased_name.cpp
+++ source/uncased_name.cpp
@@ -1,48 +1,50 @@
 #include "uncased_name.hpp"
 #include <sstream>
 
+namespace {
+
 bool last_was_upper(std::string const& str, std::size_t pos) {
-  auto last_pos = pos - 1;
-  if (last_pos >= 0 && last_pos < str.size()) {
-    return std::isupper(str[last_pos]);
-  } else {
-    return false;
-  }
+  return (pos >= 1) && std::isupper(str[pos - 1]);
+}
+
+bool last_was_underscore(std::string const& str, std::size_t pos) {
+  return (pos >= 1) && str[pos - 1] == '_';
 }
 
 bool next_is_lower(std::string const& str, std::size_t pos) {
-  auto next_pos = pos + 1;
-  if (next_pos >= 0 && next_pos < str.size()) {
-    return std::islower(str[next_pos]);
-  } else {
-    return false;
-  }
+  return (pos + 1 < str.size()) && std::islower(str[pos + 1]);
 }
+
+bool part_starts_at(std::string const& str, std::size_t pos)
+{
+  return (str[pos] != '_')
+         && (std::isupper(str[pos])
+             && (!last_was_upper(str,pos) || next_is_lower(str,pos))
+             || pos == 0
+             || last_was_underscore(str,pos));
+}
+
+bool part_continues_at(std::string const& str, std::size_t pos)
+{
+  return std::isupper(str[pos]) && last_was_upper(str,pos) && !next_is_lower(str,pos)
+         || std::islower(str[pos]);
+}
+
+} // namespace
 
 uncased_name::uncased_name(std::string const& str)
 {
   for (std::size_t i = 0; i < str.size(); ++i)
   {
-    if (std::isupper(str[i]) && (!last_was_upper(str,i) || next_is_lower(str,i)))
+    if (part_starts_at(str, i))
     {
       parts_.emplace_back(1, static_cast<char>(std::tolower(str[i])));
       continue;
     }
-    if (std::isupper(str[i]) && last_was_upper(str,i))
+    if (part_continues_at(str, i))
     {
       parts_.back().push_back(static_cast<char>(std::tolower(str[i])));
       continue;
-    }
-    if (str[i] == '_')
-    {
-      parts_.emplace_back();
-      continue;
-    }
-
-    if (parts_.empty()) {
-      parts_.emplace_back(1, str[i]);
-    } else {
-      parts_.back().push_back(str[i]);
     }
   }
 }
