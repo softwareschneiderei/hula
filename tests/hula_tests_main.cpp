a7603cfc5ea351cfe00f5866e8381165d2379653
Felix Cherubini <felix.cherubini@softwareschneiderei.de>
2020-12-16 15:34:29 +0100

    Additional test case for multiple underscores

--- tests/hula_tests_main.cpp
+++ tests/hula_tests_main.cpp
@@ -27,6 +27,11 @@ TEST_CASE("All_caps_abbreviation_ends_at_CamelCase_word_boundary")
     REQUIRE(uncased_name("ABBRe").snake_cased() == "abb_re");
 }
 
+TEST_CASE("Double_underscore_yields_the_same_as_single_underscore")
+{
+    REQUIRE(uncased_name("a__b").snake_cased() == "a_b");
+}
+
 TEST_CASE("CamelCase names can be parsed")
 {
   uncased_name x("UsedToBeCamelCase");
